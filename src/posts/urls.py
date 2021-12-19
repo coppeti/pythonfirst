aead0883452cf74d58263df74fb8f77ef52c0ffb
Etienne Coppel <eco@ik.me>
2021-12-20 00:14:20 +0100

    Navigation and css

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 
 from posts.views import BlogHome, BlogPostCreate, BlogPostEdit, BlogPostDetail, BlogPostDelete
@@ -6,8 +7,8 @@ app_name = "posts"
 
 urlpatterns = [
     path('', BlogHome.as_view(), name="home"),  # si url est 'localhost > blog/', appel la vue BlogHome
-    path('create/', BlogPostCreate.as_view(), name="create"),  # Appel le formulaire de création d'un article
-    path('edit/<str:slug>/', BlogPostEdit.as_view(), name="edit"),
+    path('create/', login_required(BlogPostCreate.as_view()), name="create"),  # Appel le formulaire de création d'un article
+    path('edit/<str:slug>/', login_required(BlogPostEdit.as_view()), name="edit"),
     path('<str:slug>/', BlogPostDetail.as_view(), name="post"),
-    path('delete/<str:slug>/', BlogPostDelete.as_view(), name="delete"),
+    path('delete/<str:slug>/', login_required(BlogPostDelete.as_view()), name="delete"),
 ]
